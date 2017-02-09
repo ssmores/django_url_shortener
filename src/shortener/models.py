5e1627c45580ef11a55cade6d0a3ee69958a9d1d
Stefanie <ssmores@hotmail.com>
2017-02-09 10:29:34 +0000

    Updated model to have KirrURL class, and updated settings.py to include new information in admin.  Removed database from this commit onward.

---
+++ src/shortener/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class KirrURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
+
+
+    def __unicode__(self):
+        return str(self.url)
