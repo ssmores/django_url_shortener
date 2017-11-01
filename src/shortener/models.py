e5a1ad2f50b3533f16159a77349e5eda141a84dd
Stefanie Moy <stefaniesmoy@gmail.com>
2017-11-01 08:05:03 +0000

    Moved generator code into a utils file.

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,20 +1,12 @@
 from __future__ import unicode_literals
-import random 
-import string
 from django.db import models
+from .utils import code_generator, create_shortcode
 
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    # new_code = ''
-    # for _ in range(size):
-    #     new_code += random.choice(char)
-    # return new_code
-
-    return ''.join(random.choice(chars) for _ in range(size))
 
 # Create your models here.
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True) #Everytime model is saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model was created
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
@@ -22,8 +14,8 @@ class KirrURL(models.Model):
 
     def save(self, *args, **kwargs):
         """Overwriting existing save method to take args and kwargs."""
-        print 'something'
-        self.shortcode = code_generator()
+        if not self.shortcode:
+            self.shortcode = create_shortcode(self)
         super(KirrURL, self).save(*args, **kwargs)
 
 
