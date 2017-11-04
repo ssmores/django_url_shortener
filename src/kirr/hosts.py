327125dd2e931211ccf5c077f82235457e527e88
Stefanie Moy <stefaniesmoy@gmail.com>
2017-11-04 00:04:28 +0000

    Added files to allow redirect to (testing URL) tirr.com, for when it starts with www or when it starts with something other than that.

---
+++ src/kirr/hosts.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django_hosts import patterns, host 
+
+host_patterns = patterns('', 
+    host(r'www', settings.ROOT_URLCONF, name='www'), 
+    host(r'(?!www).*', 'kirr.hostsconf.urls', name='wildcard'),
+    )
+
+
+# This may be a future implementation/way to do this url redirecting.
+'''
+from kirr.hostsconf import urls as redirect_urls
+
+host_patterns =  
+    host(r'www', settings.ROOT_URLCONF, name='www'), 
+    host(r'(?!www).*', redirect_urls, name='wildcard'),
+    ]
+'''
--- src/kirr/settings.py
+++ src/kirr/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'django_hosts',
+
     # Custom app
     'shortener',
 ]
 
 MIDDLEWARE = [
+    'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,9 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_hosts.middleware.HostsResponseMiddleware',
 ]
 
 ROOT_URLCONF = 'kirr.urls'
+ROOT_HOSTCONF = 'kirr.hosts'
+DEFAULT_HOST = 'www'
+DEFAULT_REDIRECT_URL = "http://www.tirr.com:80"
 
 TEMPLATES = [
     {
